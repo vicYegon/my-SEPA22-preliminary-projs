698eb2fc8729167823d65ebef26d1c67f4e7c56e
Victor Kosgei <84603202+vicYegon@users.noreply.github.com>
2022-05-20 14:07:30 +0300

    Update names starting with the letter the user enters.py

--- names starting with the letter the user enters.py
+++ names starting with the letter the user enters.py
@@ -1,9 +1,9 @@
 user_input = input(">>").title()
-names_input = []
+
 my_file = open("names.txt" , "r")
+
 def searchname():
     for s in my_file:
         if s.startswith(user_input):
-            names_input.append(s)
-    print(names_input)
+            print(s)
 searchname()
