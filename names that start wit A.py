481e9a23a3ada8ea489e898b3ea64bbcc8c744bf
Victor Kosgei <84603202+vicYegon@users.noreply.github.com>
2022-05-20 13:58:24 +0300

    Update names that start wit A.py

--- names that start wit A.py
+++ names that start wit A.py
@@ -1,10 +1,6 @@
-names_starting_with_a = []
-my_file = open("names.txt", "r")
 def search_name():
+    my_file = open("names.txt", "r")
     for s in my_file:
         if s.startswith("A"):
-            words=s.split()
-            names_starting_with_a.append(words)
-
-    print(names_starting_with_a)
+            print(s)
 search_name() 
